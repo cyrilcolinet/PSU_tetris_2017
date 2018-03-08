617d03c094534ab3a115577154b1a088334b0694
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-08 22:12:33 +0100

    :dart: Fix conflicts

--- src/map_management/display_map.c
+++ src/map_management/display_map.c
@@ -15,13 +15,14 @@ void display_map(main_t *param)
 	keypad(stdscr, TRUE);
 	noecho();
 	curs_set(FALSE);
+	use_default_colors();
 	start_color();
 	init_pair(1, COLOR_RED, -1);
-	init_pair(2, COLOR_GREEN, COLOR_BLACK);
-	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(4, COLOR_BLUE, COLOR_BLACK);
-	init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(6, COLOR_CYAN, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, -1);
+	init_pair(3, COLOR_YELLOW, -1);
+	init_pair(4, COLOR_BLUE, -1);
+	init_pair(5, COLOR_MAGENTA, -1);
+	init_pair(6, COLOR_CYAN, -1);
 	while (n != 27) {
 		clear();
 		create_tetris_title();
