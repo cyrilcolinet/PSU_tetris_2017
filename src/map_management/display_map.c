0b1e5ef3f2d6e07f988b1c102c71c818424ade05
Marcel Lucas <lucas.marcel@epitech.eu>
2018-03-08 22:16:38 +0100

    fix conflit

--- src/map_management/display_map.c
+++ src/map_management/display_map.c
@@ -9,25 +9,8 @@
 
 void display_map(void)
 {
-	int n = 1;
-
-	initscr();
-	keypad(stdscr, TRUE);
-	noecho();
-	curs_set(FALSE);
-	use_default_colors();
-	start_color();
-	init_pair(1, COLOR_RED, -1);
-	init_pair(2, COLOR_GREEN, -1);
-	init_pair(3, COLOR_YELLOW, -1);
-	init_pair(4, COLOR_BLUE, -1);
-	init_pair(5, COLOR_MAGENTA, -1);
-	init_pair(6, COLOR_CYAN, -1);
-	while (n != 27) {
-		clear();
-		create_tetris_title();
-		refresh();
-		n = getch();
-	}
-	endwin();
+	int x = 20;
+	int y = 10;
+	//33
+	
 }
