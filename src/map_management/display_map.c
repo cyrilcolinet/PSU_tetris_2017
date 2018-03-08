26465b487e54137cbcacc66a98b85f1bad28a6a6
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-08 22:10:00 +0100

    Fix colors

--- src/map_management/display_map.c
+++ src/map_management/display_map.c
@@ -13,10 +13,10 @@ void display_map(main_t *param)
 
 	initscr();
 	keypad(stdscr, TRUE);
-	raw();
 	noecho();
+	use_default_colors();
 	start_color();
-	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(1, COLOR_RED, -1);
 	init_pair(2, COLOR_GREEN, COLOR_BLACK);
 	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(4, COLOR_BLUE, COLOR_BLACK);
