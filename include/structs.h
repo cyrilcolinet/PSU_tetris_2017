fd05cc9e2b6611b9ad6a577bff563e2e6ec587ef
Marcel Lucas <lucas.marcel@epitech.eu>
2018-03-08 14:22:52 +0100

    add struct

--- include/structs.h
+++ include/structs.h
@@ -14,4 +14,19 @@
 # include <fcntl.h>
 # include <ncurses.h>
 
+typedef struct tetriminos_s {
+	char *name;
+	char **form;
+	struct tetriminos *next;
+} tetriminos_t;
+
+typedef struct config_s {
+	
+} config_t;
+
+typedef struct main_s {
+	tetriminos_t *tetri;
+	config_t config;
+} main_t;
+
 # endif
