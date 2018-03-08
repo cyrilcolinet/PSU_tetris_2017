b96bb67ba1d481fd3888e6c642820c645bf3af5b
Marcel Lucas <lucas.marcel@epitech.eu>
2018-03-08 22:03:51 +0100

    add color

--- src/map_management/display_map.c
+++ src/map_management/display_map.c
@@ -13,8 +13,8 @@ void display_map(main_t *param)
 
 	initscr();
 	keypad(stdscr, TRUE);
-	raw();
 	noecho();
+	curs_set(FALSE);
 	start_color();
 	init_pair(1, COLOR_RED, COLOR_BLACK);
 	init_pair(2, COLOR_GREEN, COLOR_BLACK);
@@ -24,7 +24,6 @@ void display_map(main_t *param)
 	init_pair(6, COLOR_CYAN, COLOR_BLACK);
 	while (n != 27) {
 		clear();
-		curs_set(FALSE);
 		create_tetris_title();
 		refresh();
 		n = getch();
