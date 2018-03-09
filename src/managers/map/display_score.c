05c55d61842f7ef4bd26c4ed3d829a8fe3fe3c51
Marcel Lucas <lucas.marcel@epitech.eu>
2018-03-09 15:40:37 +0100

    score affichagre terminer

--- src/managers/map/display_score.c
+++ src/managers/map/display_score.c
@@ -7,13 +7,29 @@
 
 #include "tetris.h"
 
+static int nb_length(int nb)
+{
+	int n = 0;
+
+	while (nb / 10 > 0) {
+		nb = nb / 10;
+		n++;
+	}
+	return (n);
+}
+
 static void display_value_score()
 {
-	mvprintw( 10, 3, "High Score");
-	mvprintw( 11, 3, "Score");
-	mvprintw( 13, 3, "Lines");
-	mvprintw( 14, 3, "Level");
-	mvprintw( 16, 3, "Timer");
+	int nb = nb_length(100);
+	mvprintw( 10, 26 - nb, "100");
+	nb = nb_length(1);
+	mvprintw( 11, 26 - nb, "1");
+	nb = nb_length(4957385);
+	mvprintw( 13, 26 - nb, "4957385");
+	nb = nb_length(2487);
+	mvprintw( 14, 26 - nb, "2487");
+	nb = nb_length(0);
+	mvprintw( 16, 26 - nb, "0");
 }
 
 void display_score(void)
